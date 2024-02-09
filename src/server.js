6a7e9ad8445f8e9f23330dec69f97f7530f0ba1c
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-09 14:36:14 -0300

    setup sequelize for development environment

--- src/server.js
+++ src/server.js
@@ -1,22 +1,45 @@
 import express from "express";
 import cors from "cors";
 import cookieSession from "cookie-session";
+import db from "./models/index.js";
 
-var app = express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(cookieSession({
-  name: "session",
-  secret: "INNOVA-CONNECT-TEST-SECRET-KEY",
-  httpOnly: true,
-  maxAge: 24 * 60 * 60 * 1000
-}));
+app.use(
+  cookieSession({
+    name: "session",
+    secret: "INNOVA-CONNECT-TEST-SECRET-KEY",
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  })
+);
+
+// For the purpose of this job assessment, we will drop and re-sync the database 
+// each time the application starts.
+// For production, only sync() is used.
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+  initial();
+});
 
 app.get("/", cors(), (req, res) => {
   res.status(200).json({ data: { message: "User data" }});
 });
+
+function initial() {
+  db.role.create({
+    id: 1,
+    name: "USER"
+  });
+  
+  db.role.create({
+    id: 2,
+    name: "ADMIN"
+  });
+}
 
 export default app;
