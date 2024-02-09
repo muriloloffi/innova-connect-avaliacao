3f7a0f8895abd7f1610b34960dfd888e490e8537
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-09 13:26:58 -0300

    setup middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import express from "express";
+import cors from "cors";
+import cookieSession from "cookie-session";
 
 var app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+app.use(express.json());
+app.use(cors())
+app.use(cookieSession({
+  name: "session",
+  secret: "innova-connect-secret",
+  maxAge: 24 * 60 * 60 * 1000
+}))
+
+app.get("/", cors(), (req, res) => {
+  res.status(200).json({ data: { message: "User data" }});
 });
 
 export default app;
