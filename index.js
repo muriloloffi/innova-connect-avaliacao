5b704eefe78726580b58b97b8cd03971ede37e47
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-09 13:53:30 -0300

    add: urlencoding middleware, db config file, port constant variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import app from "./src/app.js";
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
 });
--- src/app.js
+++ src/app.js
@@ -5,12 +5,15 @@ import cookieSession from "cookie-session";
 var app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 app.use(cookieSession({
   name: "session",
-  secret: "innova-connect-secret",
+  secret: "INNOVA-CONNECT-TEST-SECRET-KEY",
+  httpOnly: true,
   maxAge: 24 * 60 * 60 * 1000
-}))
+}));
 
 app.get("/", cors(), (req, res) => {
   res.status(200).json({ data: { message: "User data" }});
