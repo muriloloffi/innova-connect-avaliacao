b8b8b2174a4e8f57a100380f5a608b0ac4162d98
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-13 14:14:46 -0300

    remove auto generated comments

--- src/models/checkin.js
+++ src/models/checkin.js
@@ -8,12 +8,6 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class Checkin extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method
-     * automatically.
-     */
     static associate(models) {
       Checkin.belongsTo(models.User, {
         foreignKey: 'user_id',
