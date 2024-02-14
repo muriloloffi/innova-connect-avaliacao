f55e5134285586d4d754db4b045a5b9c8be8772a
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-13 22:38:14 -0300

    business rule: one daily checkin only

--- src/services/CheckinServices.js
+++ src/services/CheckinServices.js
@@ -1,8 +1,35 @@
+const { Op } = require('sequelize');
 const Services = require('./Services.js');
+const { Checkin } = require('../models/index.js');
+
+const TODAY_START = new Date().setHours(0, 0, 0, 0);
+const TODAY_END = new Date().setHours(23, 59, 59, 999);
 
 class CheckinServices extends Services {
   constructor() {
     super('Checkin');
+  }
+
+  async createRecord(checkinData) {
+    const dailyCheckins = await Checkin.findAndCountAll({
+      where: {
+        [Op.and]: [
+          { user_id: checkinData.user_id },
+          {
+            createdAt: {
+              [Op.gte]: TODAY_START,
+              [Op.lte]: TODAY_END,
+            },
+          },
+        ],
+      },
+    });
+
+    if (dailyCheckins.count >= 1) {
+      throw new Error('User already checked in today');
+    }
+
+    return super.createRecord(checkinData);
   }
 }
 
