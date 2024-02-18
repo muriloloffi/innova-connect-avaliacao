14fc84a6c4e8c18a9bd038f216fe44e10f043cbf
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-17 22:31:49 -0300

    WIP session store

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,34 @@
+/* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
-const cookieSession = require('cookie-session');
+const session = require('express-session');
+const redis = require('redis');
+const RedisStore = require('connect-redis').default;
 const users = require('./usersRoutes.js');
 const gyms = require('./gymsRoutes.js');
 const auth = require('./authRoutes.js');
 
+const redisClient = redis.createClient({
+  url: 'redis://redis:6379',
+});
+redisClient.connect().catch(console.error);
+
+const redisStore = new RedisStore({ client: redisClient });
+
 module.exports = (app) => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use(
-    express.json(),
     users,
     gyms,
     auth,
   );
-  app.use(express.urlencoded({ extended: true }));
   app.use(cors({ origin: 'http://localhost:3000' }));
   app.use(
-    cookieSession({
-      name: 'session',
+    session({
+      store: redisStore,
       secret: 'INNOVA-CONNECT-TEST-SECRET-KEY',
+      name: 'session',
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
     }),
