4590010c73217cbeeba73417050071699ca051b8
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-19 14:36:36 -0300

    WIP fix Cross-Origin Resource Sharing (CORS)

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,12 +8,12 @@ const users = require('./usersRoutes.js');
 const gyms = require('./gymsRoutes.js');
 const auth = require('./authRoutes.js');
 
-const redisClient = redis.createClient({
-  url: 'redis://redis:6379',
-});
-redisClient.connect().catch(console.error);
+// const redisClient = redis.createClient({
+//   url: 'redis://redis:6379',
+// });
+// redisClient.connect().catch(console.error);
 
-const redisStore = new RedisStore({ client: redisClient });
+// const redisStore = new RedisStore({ client: redisClient });
 
 module.exports = (app) => {
   app.use(express.json());
@@ -23,10 +23,10 @@ module.exports = (app) => {
     gyms,
     auth,
   );
-  app.use(cors({ origin: 'http://localhost:5173' }));
+  app.use(cors({ origin: 'http://localhost:5173/' }));
   app.use(
     session({
-      store: redisStore,
+      // store: redisStore,
       secret: 'INNOVA-CONNECT-TEST-SECRET-KEY',
       name: 'session',
       httpOnly: true,
